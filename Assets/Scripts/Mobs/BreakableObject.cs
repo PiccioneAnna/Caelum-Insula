900ac9297d65f6d42c7cd3bace56d3157025876a
SexiSalmon <78030955+PiccioneAnna@users.noreply.github.com>
2024-02-04 23:38:08 +0000

    begining of mobs

---
+++ Assets/Scripts/Mobs/BreakableObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Damageable))]
+public class BreakableObject : MonoBehaviour, IDamageable
+{
+    [SerializeField] int hp = 10;
+
+    public void ApplyDamage(int damage)
+    {
+        hp -= damage;
+    }
+
+    public void CalculateDamage(ref int damage)
+    {
+        damage /= 2;
+    }
+
+    public void CheckState()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
