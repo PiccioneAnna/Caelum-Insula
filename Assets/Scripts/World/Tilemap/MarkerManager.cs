2d88b0749ccce2438c859176da949427a62c1e7e
SexiSalmon <78030955+PiccioneAnna@users.noreply.github.com>
2024-03-15 20:16:22 +0000

    mlem

--- Assets/Scripts/World/Tilemap/MarkerManager.cs
+++ Assets/Scripts/World/Tilemap/MarkerManager.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.Timeline;
-using UnityEngine.WSA;
 
 namespace TilemapScripts
 {
